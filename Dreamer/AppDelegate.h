add3a2c1055273162928c0be6b2eafe6940f4434
Eric Rhodes <erhodes71@gmail.com>
2018-07-16 13:34:10 -0500

    Initial Commit

---
+++ Dreamer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dreamer
+//
+//  Created by Eric Rhodes on 7/16/18.
+//  Copyright © 2018 Eric Rhodes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
